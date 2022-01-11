a29dd6ebc8e778ea3279b289c1fd9cbee53d97db
Aayush Neupane <neupane0403@gmail.com>
2022-01-11 11:16:41 +0545

    simple universal sentence encoder

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # realtime_text_similarity_backend
 Backend for realtime text semantic similarity identification
 
-#TODO:
-1. create simple backend that handle simple post request and send json response
-2. Use simple algorithm for semantic similarity identification
+# How to run?
+1. set git hooks path
+    `git config core.hooksPath .githooks`
+2. start flask server
+
+3. post request in localhost:5000 with json body which contains "question"
+```
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"question":"who is the father of economics?"}' \
+  http://localhost:5000
+  ```
--- app.py
+++ app.py
@@ -1,18 +1,55 @@
-from flask import Flask
-
+from flask import Flask, request
 from flask_restful import Resource, Api
+import tensorflow_hub as hub
+import numpy as np
+
+module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+model = hub.load(module_url)
+print("module %s loaded" % module_url)
+
+question_database = [
+    "What is AVL Tree?",
+    "Describe 4 principle of economics.",
+    "Differentiate between process and threads",
+    "Describe UML diagram and create one for online shopping system.",
+    "Descrive VHDL in detail.",
+    "Differentiate between dfs and bfs",
+]
+
+
+def embed(input):
+    return model(input)
+
+
+def create_dict(score, questions):
+    return dict(zip(score, questions))
+
+
+def get_similar(question):
+    inp = [question] + question_database
+
+    embeddings = embed([question] + question_database)
+    distances = list(np.inner(embeddings, embeddings)[0][1:])
+    similarity = create_dict(question_database, distances)
+    return dict(sorted(similarity.items(), key=lambda x: x[1], reverse=True))
 
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource ):
+class Similarity(Resource):
     def get(self):
         return {"hello": "world"}
 
+    def post(self):
+        json_data = request.get_json(force=True)
+        qn = json_data["question"]
+        similarity = get_similar(qn)
+        x = {key: str(val) for key, val in similarity.items()}
+        return x
 
-api.add_resource(HelloWorld, "/")
 
+api.add_resource(Similarity, "/")
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
