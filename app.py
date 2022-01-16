b431c253162979cb88636082535189abba4dca1d
Aayush Neupane <neupane0403@gmail.com>
2022-01-16 20:52:45 +0545

    CORS issue

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from algorithms import get_similar
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
 
