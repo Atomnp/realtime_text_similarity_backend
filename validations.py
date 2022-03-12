edefd9fdbad85d09e1c17a856ae3061ddf4b7796
Aayush Lamichhane <lamichhaneaayush5@gmail.com>
2022-03-12 20:05:12 +0545

    word2vec validation working

--- validations.py
+++ validations.py
@@ -5,22 +5,50 @@ from encoders.use import UniversalEncoder
 from encoders.bert import BERT
 from encoders.word2vec import word2vec
 
-filename = "/home/smloy/Downloads/dataset (2).txt"
+filename = "./datasets/validate.txt"
 
-def get_accuracy(encoder,sentences):
-    embeddings = encoder.encode_array([question[0]+"?" for question in questions])
+
+def get_accuracy(encoder, sentences):
+    embeddings = encoder.encode_array([question[0] + "?" for question in questions])
     annoy_index = AnnoyIndex(dimension=len(embeddings[0]))
     annoy_index.build(embeddings, [question[1] for question in questions])
     accuracy = 0
-        
+
     for question, unique_id in questions:
         no_of_similar_sent = [question[1] for question in questions].count(unique_id)
         neighbours = annoy_index.query(encoder.encode(question), k=no_of_similar_sent)
         # print(neighbours)
         true_similar_count = neighbours.count(unique_id)
         accuracy += true_similar_count / no_of_similar_sent
-    
-    return accuracy/len(embeddings)*100
+
+    return accuracy / len(embeddings) * 100
+
+
+from gensim.models.callbacks import CallbackAny2Vec
+
+
+def identity_tokenizer(text):
+    return text
+
+
+class callback(CallbackAny2Vec):
+    """Callback to print loss after each epoch."""
+
+    def __init__(self):
+        self.epoch = 0
+
+    def on_epoch_end(self, model):
+        loss = model.get_latest_training_loss()
+        if self.epoch == 0:
+            print("Loss after epoch {}: {}".format(self.epoch, loss))
+        else:
+            print(
+                "Loss after epoch {}: {}".format(
+                    self.epoch, loss - self.loss_previous_step
+                )
+            )
+        self.epoch += 1
+        self.loss_previous_step = loss
 
 
 if __name__ == "__main__":
@@ -32,21 +60,24 @@ if __name__ == "__main__":
         len(questions[0]) == 2
     ), "first item should be sentence second item shoud be number representing unique question id"
     # encoder = UniversalEncoder(model_path="./USE")
-    question_array=[question[0]+"?" for question in questions]
+    question_array = [question[0] + "?" for question in questions]
 
-    bert_accuracy=get_accuracy(BERT(),question_array)
-    arora_accuracy=get_accuracy(Arora(),question_array)
-    use_accuracy=get_accuracy(UniversalEncoder(),question_array)
-    # word2vec_accuracy=get_accuracy(word2vec(),question_array)
-
-    print("bert_accuracy: {0}\n arora_accuracy: {1}\n use_accuracy: {2}".format(bert_accuracy,arora_accuracy,use_accuracy))
+    bert_accuracy = get_accuracy(BERT(), question_array)
+    arora_accuracy = get_accuracy(Arora(), question_array)
+    use_accuracy = get_accuracy(UniversalEncoder(), question_array)
+    word2vec_accuracy = get_accuracy(word2vec(), question_array)
+    print(
+        "bert_accuracy: {0}\n arora_accuracy: {1}\n use_accuracy: {2}\n word2vec_accuracy: {3}".format(
+            bert_accuracy, arora_accuracy, use_accuracy, word2vec_accuracy
+        )
+    )
 
     # encoder = BERT()
     # embeddings = encoder.encode_array(question_array)
     # annoy_index = AnnoyIndex(dimension=len(embeddings[0]))
     # annoy_index.build(embeddings, [question[1] for question in questions])
     # accuracy = 0
-        
+
     # for question, unique_id in questions:
     #     no_of_similar_sent = [question[1] for question in questions].count(unique_id)
     #     neighbours = annoy_index.query(encoder.encode(question), k=no_of_similar_sent)
