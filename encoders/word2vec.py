b63073d66a1bbff92d1e0e3ce5b6d2defdbd381a
Aayush Lamichhane <lamichhaneaayush5@gmail.com>
2022-03-12 10:07:25 +0545

    implemented encode_array for tfidf_w2v

--- encoders/word2vec.py
+++ encoders/word2vec.py
@@ -1,4 +1,3 @@
-# from gensim.models.word2vec import Word2Vec
 from gensim.models import Word2Vec
 from encoders.base import Encoder
 import numpy as np
@@ -32,8 +31,38 @@ class word2vec(Encoder):
         arrlist = np.array(list(map(lambda x: self.w2v_model.wv[x[0]], filtered[:5])))
 
         sentence_embedding = np.mean(arrlist, axis=0)
-        
+
         if type(sentence_embedding) == np.ndarray:
             return sentence_embedding
         else:
             return np.random.randn(embedding_size)
+
+    def encode_array(self, sentences: str, embedding_size=100):
+        embeddings = []
+        for sentence in sentences:
+            to_transform = word_tokenize(sentence)
+            matrix = self.vect.transform([to_transform])
+            cx = coo_matrix(matrix)
+
+            sorted_by_tfidf = sorted(
+                [(self.fv[j], v) for i, j, v in zip(cx.row, cx.col, cx.data)],
+                key=lambda x: x[1],
+                reverse=True,
+            )
+            filtered = list(
+                filter(lambda x: x[0] in self.w2v_model.wv, sorted_by_tfidf)
+            )
+            if filtered == []:
+                embeddings.append(np.random.randn(embedding_size))
+                break
+            arrlist = np.array(
+                list(map(lambda x: self.w2v_model.wv[x[0]], filtered[:5]))
+            )
+
+            sentence_embedding = np.mean(arrlist, axis=0)
+
+            if type(sentence_embedding) == np.ndarray:
+                embeddings.append(sentence_embedding)
+            else:
+                embeddings.append(np.random.randn(embedding_size))
+        return embeddings
