60b1a7905af9d455a314c7c64b6f10e1b2f5b530
Ankit Paudel <ankitpaudel20000@gmail.com>
2022-03-18 21:39:43 +0545

    fixed validation functions

--- validations.py
+++ validations.py
@@ -5,10 +5,11 @@ from encoders.use import UniversalEncoder
 from encoders.bert import BERT
 from encoders.word2vec import word2vec
 
-filename = "./datasets/validate.txt"
+filename = "./uploads/dataset (2).txt"
 
 
 def get_accuracy(encoder, sentences):
+
     embeddings = encoder.encode_array([question[0] + "?" for question in questions])
     annoy_index = AnnoyIndex(dimension=len(embeddings[0]))
     annoy_index.build(embeddings, [question[1] for question in questions])
@@ -23,6 +24,27 @@ def get_accuracy(encoder, sentences):
 
     return accuracy / len(embeddings) * 100
 
+def get_accuracy_mrr(encoder, sentences):
+
+    embeddings = encoder.encode_array([question[0] + "?" for question in questions])
+    annoy_index = AnnoyIndex(dimension=len(embeddings[0]))
+    annoy_index.build(embeddings, [question[1] for question in questions])
+    accuracy = 0
+
+    for question, unique_id in questions:
+        no_of_similar_sent = [question[1] for question in questions].count(unique_id)
+        neighbours = annoy_index.query(encoder.encode(question), k=no_of_similar_sent)
+        # print(neighbours)
+        denominator=0
+        numerator=0
+        for i in range(no_of_similar_sent):
+            denominator+=1/(i+1)
+            if neighbours[i]==unique_id:
+                numerator += 1/(i+1)
+
+        accuracy += numerator / denominator
+
+    return accuracy/len(embeddings)*100
 
 from gensim.models.callbacks import CallbackAny2Vec
 
@@ -72,33 +94,12 @@ if __name__ == "__main__":
         )
     )
 
-    # encoder = BERT()
-    # embeddings = encoder.encode_array(question_array)
-    # annoy_index = AnnoyIndex(dimension=len(embeddings[0]))
-    # annoy_index.build(embeddings, [question[1] for question in questions])
-    # accuracy = 0
-
-    # for question, unique_id in questions:
-    #     no_of_similar_sent = [question[1] for question in questions].count(unique_id)
-    #     neighbours = annoy_index.query(encoder.encode(question), k=no_of_similar_sent)
-    #     print(neighbours)
-    #     true_similar_count = neighbours.count(unique_id)
-    #     accuracy += true_similar_count / no_of_similar_sent
-
-    # for question, unique_id in questions:
-    #     no_of_similar_sent = [question[1] for question in questions].count(unique_id)
-    #     neighbours = annoy_index.query(encoder.encode(question), k=no_of_similar_sent)
-    #     print(neighbours)
-    #     denominator=0
-    #     numerator=0
-    #     for i in range(no_of_similar_sent):
-    #         denominator+=1/(i+1)
-    #         if neighbours[i]==unique_id:
-    #             numerator += 1/(i+1)
-
-    #     accuracy += numerator / denominator
-
-    # print(f"accuracy={accuracy/len(embeddings)*100}")
-
-    # annoy_index.save("./indices/" + filename + str(self.current_algo) + ".ann")
-    # self.index = annoy_index
+    bert_accuracy = get_accuracy_mrr(BERT(), question_array)
+    arora_accuracy = get_accuracy_mrr(Arora(), question_array)
+    use_accuracy = get_accuracy_mrr(UniversalEncoder(), question_array)
+    word2vec_accuracy = get_accuracy_mrr(word2vec(), question_array)
+    print(
+        "bert_accuracy: {0}\n arora_accuracy: {1}\n use_accuracy: {2}\n word2vec_accuracy: {3}".format(
+            bert_accuracy, arora_accuracy, use_accuracy, word2vec_accuracy
+        )
+    )
